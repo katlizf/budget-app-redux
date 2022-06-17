653279595c20c1e3460f9cec78e29666c0e97a98
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-17 16:10:00 -0500

    step 5 completed

--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -1,3 +1,6 @@
+import axios from 'axios'
+
+
 const initialState = {
     email: null,
     firstName: null,
@@ -5,7 +8,23 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    return state
+    switch (action.type) {
+        case REQUEST_USER_DATA + '_FULFILLED':
+            const { email, firstName, lastName } = action.payload.user
+            return { email, firstName, lastName }
+        default:
+        return state
+    }    
+}
+
+let REQUEST_USER_DATA = 'REQUEST_USER_DATA'
+
+export const requestUserData = () => {
+    const data = axios.get('/auth/user-data').then(res => res.data)
+    return {
+        type: REQUEST_USER_DATA,
+        payload: data
+    }
 }
 
 export default reducer
